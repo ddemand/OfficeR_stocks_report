fd89d946735a266bbd7786cccdc62896248f1564
David Demand <ddeman02@gmail.com>
2020-02-15 20:47:51 -0600

    Updated code to accomodate a new presentation theme and corrected layouts.

--- create_ppt.R
+++ create_ppt.R
@@ -29,8 +29,10 @@ library("dygraphs") # A package for interactive plots
 if (!require(DT)) install.packages('DT')
 library("DT")
 
+# ===== Set current working directory
+setwd("~/R/Projects/OfficeR_stocks_report")
 
-# ===== Using the next line to get stocks from stooq.com 
+# ===== Get stocks using stooq.com 
 
 # **Choose different stocks by changing only the "symbol" variable**
 ### ======== ###
@@ -43,16 +45,11 @@ df <- read.csv(URL)
 # The function 'str()' provides a glimpse of the structure for the dataframe
 str(df)
 
-# Conversion of date intervals using the lubridate package
-df$Date <- ymd(df$Date)
-df$Month <- month(df$Date, abbr = FALSE)
-df$Quarter <- quarter(df$Date)
-df$Spread <- df$High - df$Low
-
 # Assembling data using dplyr.  Select the 'df' element from the Global Environent to see the entire dataset
 df <- df %>%
   mutate(Date = ymd(Date),
-         Month = month(Date, abbr=FALSE),
+         Month = month(Date, label = TRUE, abbr = TRUE, locale = Sys.getlocale("LC_TIME")),
+         Mon = month(Date, label = FALSE, abbr = TRUE, locale = Sys.getlocale("LC_TIME")),
          Year = year(Date),
          Quarter = quarter(Date),
          Spread = (High - Low)
@@ -72,6 +69,13 @@ Quarterly <- df %>%
   mutate(QoQ = Close - lag(Close)) %>%
   arrange(Year)
 
+# Create a monthly dataset from the 'df' datafram
+Monthly <- df %>%
+  select(Close,Year, Month,Mon) %>%
+  group_by(Year, Month) %>%
+  summarise_all(funs(median(as.numeric(.)))) %>%
+  mutate(MoM = Close - lag(Close)) %>%
+  arrange(Month,Year)
 
 # ===== Create various interactive plots of the data using the 'plotly' package
 daily <- ggplot(data = df, aes(x=Date, y=Close)) + geom_line()
@@ -89,11 +93,12 @@ ggsave(
 )
 
 # Historical daily, faceted by the avaialable years
-yearly <- ggplot(data = df, aes(x=Date, y=Close)) + geom_line() + facet_wrap(~ year(Date), scales = "free") +
+yearly <- ggplot(data = df, aes(x=Date, y=Close)) + geom_line() + 
+  facet_wrap(~ year(Date), scales = "free") +
   theme(axis.text.x = element_text(face="plain", color="black",
-                                   size=8, angle=-90),
+                                   size=6, angle=-90),
         axis.text.y = element_text(face="plain", color="black",
-                                   size=8, angle=0)) +
+                                   size=6, angle=0)) +
   geom_smooth(method = loess, se = FALSE, size =.5)
 ggplotly(yearly)
 yearly <- ggpubr::ggarrange(yearly)
@@ -106,34 +111,26 @@ ggsave(
 )
 
 #Boxplots for each month of each year
-ggplot(df, aes(x = Date, y = Close, group = interaction( month(Date)))) + 
-  geom_boxplot()
-
-bp <- ggplot(plotbycity, aes(x = date, y = rent, group = city, color = city,
-    text = paste('<br>Date: ', as.Date(date),
-                 '<br>Obs: ', count))) +
-                 geom_boxplot(data = df, aes(x=Date, y=Close))  
+monthly_bp <- ggplot(Monthly, aes(x = Month, y = Close, group = interaction( Month))) + 
+  geom_boxplot() +
   theme(axis.text.x = element_text(face="plain", color="black",
                                    size=8, angle=-90),
         axis.text.y = element_text(face="plain", color="black",
-                                   size=8, angle=0))
-ggplotly(bp)
-
-# Deviations
-df_SD <- sd(df$Close)
-df_SpreadSD <- sd(df$Spread)
-
-# Empirical Rule Upper and Lower bounds of the distribution
-Lower1SD68 <- round(mean(df$Close) - sd(df$Close),2)
-Upper1SD68 <- round(mean(df$Close) + sd(df$Close),2)
-Lower2SD95 <- round(mean(df$Close) - (sd(df$Close)*2),2)
-Upper2SD95 <- round(mean(df$Close) + (sd(df$Close)*2),2)
-Lower3SD99p7 <- round(mean(df$Close) - (sd(df$Close)*3),2)
-Upper3SD99p7 <- round(mean(df$Close) + (sd(df$Close)*3),2)
+                                   size=8, angle=0)) + 
+  scale_x_discrete(breaks=as.character(unique(unlist(Monthly$Month))),
+                   labels=as.character(unique(unlist(Monthly$Month))))
+ggplotly(monthly_bp)
+ggsave(
+  "monthly.png",
+  monthly_bp,
+  width = 7,
+  height = 5,
+  dpi = 1200
+)
 
 # Histogram with density plot of all closing prices
 histo <- ggplot(df, aes(x=Close )) +
-  geom_histogram(aes(y=..density..), colour="black", fill="white",bins =25)+
+  geom_histogram(aes(y=..density..), colour="black", fill="white",bins =30)+
   geom_density(alpha=.2, fill="#0FA2C7")
 ggplotly(histo)
 ggsave(
@@ -147,7 +144,9 @@ ggsave(
 
 # ===== The following chunck leverages the Facebook Prophet package to provide simple forecasts of closing prices
 # Use the following line if you need to apply filtering beyond any specific date
-df2 <- filter(df,Date >= '2016-11-08')
+# df2 <- filter(df,Date >= '2016-11-08')
+# ...or by a certain number of days back. In this case, three years back
+df2 <- filter(df,Date >= (Sys.Date())-365*3 )
 
 # Create the date series, y-axis values
 ds <- as.POSIXct(df2$Date)
@@ -202,15 +201,19 @@ ggplotly(fcst_plt)
 # ====== Prepare and complete the PowerPoint Presentation Deck using methods learned from https://davidgohel.github.io/officer
 
 # PowerPoint Comments and Variables
-cap1 <- paste0("In this chart we can see the histogram of all daily closings. The median close is: $", median(df$Close))
-cap2 <- "In this chart we can see the historical closings for each day and the respective trend."
-cap3 <- "In this chart we can see the historical closings for each day of each year and the respective trend."
-cap4 <- "In this chart we can see the projected forecasts."
-cap5 <- "In this chart we can see the forecast components."
-myftr <- paste0("Contains confidential company information. Not for external use or disclosure without proper authorization. Created on: ", Sys.Date())
-median_close <- paste0("The median close is:", median(df$Close))
+cap1 <- paste0("In this chart we can see the histogram of all ",symbol," daily closings. The median close is: $", median(df$Close))
+cap2 <- paste0("The chart for this slide illustrates the historical closings for each day and the respective trend for ", symbol, ".")
+cap3 <- paste0("Here we can see the historical closings for each day of each year and the respective trend of ", symbol, " stock price closings.")
+cap4 <- paste0("The Monthly Boxplot's show the density for variances, the spread and medain of the closings of each respective month for", symbol)
+cap5 <- paste0("This is the projected one year forecasts for ", symbol)
+cap6 <- paste0("In this illustration we can see the ", symbol, " forecast components.")
+myftr <- paste0("Contains confidential ", symbol," company information (Not really...). Not for external use or disclosure without proper authorization. Created on: ", Sys.Date())
+median_close <- paste0("The median close is: $", median(df$Close), ". Yesterday's close of $", 
+                       tail(df2$Close,1), " is a ", (tail(df2$Close,1)-(median(df$Close))/(median(df$Close)) *100), 
+                       "% change from the median and a ", round((((tail(df2$Close,1)-(tail(df2$Close,2)[1]))/(tail(df2$Close,2)[1])) *100),3), 
+                       "% change from the previous days close at $", tail(df2$Close,2)[1], ".")
 close_summary <- print(summary(df$Close))
-yesterday <- autofit(flextable(head(df,1)))
+yesterday <- autofit(flextable(tail(df,1)))
 
 # Read in MS PowerPoint Deck with OfficeR
 my_ppt <- officer::read_pptx("company.pptx")
@@ -223,45 +226,56 @@ props
 
 # Title Slide
 my_ppt2 <- on_slide( x= my_ppt, index= 1) %>%
-  ph_with(value= paste0("Historical stock prices on : ",symbol), location= ph_location_type(type = "title")) %>%
+  ph_with(value= paste0("Historical stock prices on : ",symbol), location= ph_location_type(type = "ctrTitle")) %>%
+  ph_with(value= paste0("A simple example of creating a PowerPoint presentation using R for the stock prices of ", symbol, "."), 
+          location= ph_location_type(type = "subTitle")) %>%
   ph_with(value= myftr, location= ph_location_type(type = "ftr")) %>%
-  add_slide(., layout= "Title and Content", master= "Crop") %>%
-  ph_with_text(type = "title", index=1,str = paste0("Yesterday's ", symbol, " Stock Price Summary: ")) %>%
+  
+  #Slide 2  
+  add_slide(., layout= "Title and Content", master= "Circuit") %>%
+  ph_with_text(type = "title", index=1,str = paste0("The Latest Available ", symbol, "Stock Price Summary: ")) %>%
   ph_with(., yesterday, location = ph_location_type(type = "body"), use_loc_size = TRUE ) %>%
-
-# Slide 2
-  add_slide(., layout= "Content with Caption", master= "Crop") %>%
+  
+  # Slide 3
+  add_slide(., layout= "Content with Caption", master= "Circuit") %>%
   ph_with_text(type = "title", index=1,str = paste0("Histogram Plot of the ", symbol, " Daily Closings")) %>%
-  ph_with_text(type = "body", index=1,str = cap1) %>%  
-  ph_with(., value= external_img("/Users/daviddemand/R/Projects/SimpleStocksReport/histo.png", width = 6.75, height = 5.33),
-          location = ph_location_type(type = "body", index=2), use_loc_size = FALSE ) %>%  
-
-# Slide 3
-  add_slide(., layout= "Content with Caption", master= "Crop") %>%
+  ph_with_text(type = "body", index=2,str = cap1) %>%  
+  ph_with(., value= external_img("~/R/Projects/OfficeR_stocks_report/histo.png", width = 7, height = 5),
+          location = ph_location_type(type = "body", index=1), use_loc_size = FALSE ) %>%  
+  
+  # Slide 4
+  add_slide(., layout= "Content with Caption", master= "Circuit") %>%
   ph_with_text(type = "title", index=1,str = "Historical Daily Closings with Trendline") %>%
-  ph_with_text(type = "body", index=1,str = cap2) %>%  
-  ph_with(., value= external_img("/Users/daviddemand/R/Projects/SimpleStocksReport/daily_tl.png", width = 6.75, height = 5.33),
-          location = ph_location_type(type = "body", index=2), use_loc_size = FALSE ) %>%
-
-# Slide 4
-  add_slide(., layout= "Content with Caption", master= "Crop") %>%
+  ph_with_text(type = "body", index=2,str = cap2) %>%  
+  ph_with(., value= external_img("~/R/Projects/OfficeR_stocks_report/daily_tl.png", width = 7, height = 5),
+          location = ph_location_type(type = "body", index=1), use_loc_size = FALSE ) %>%
+  
+  # Slide 5
+  add_slide(., layout= "Content with Caption", master= "Circuit") %>%
   ph_with_text(type = "title", index=1,str = "Historical Daily Closings with Trendline: Yearly Facet") %>%
-  ph_with_text(type = "body", index=1,str = cap3) %>%  
-  ph_with(., value= external_img("/Users/daviddemand/R/Projects/SimpleStocksReport/yearly.png", width = 6.75, height = 5.33),
-          location = ph_location_type(type = "body", index=2), use_loc_size = FALSE ) %>%
- 
-# Slide 5
-  add_slide(., layout= "Content with Caption", master= "Crop") %>%
-  ph_with_text(type = "title", index=1,str = "Forecasted Closing Prices using FB Prophet") %>%
-  ph_with_text(type = "body", index=1,str = cap4) %>%
-  ph_with(., value= external_img("/Users/daviddemand/R/Projects/SimpleStocksReport/forecast.png", width = 6.75, height = 5.33),
-          location = ph_location_type(type = "body", index=2), use_loc_size = FALSE ) %>%    
-
-# Slide 6
-  add_slide(., layout= "Content with Caption", master= "Crop") %>%
+  ph_with_text(type = "body", index=2,str = cap3) %>%  
+  ph_with(., value= external_img("~/R/Projects/OfficeR_stocks_report/yearly.png", width = 7, height = 5),
+          location = ph_location_type(type = "body", index=1), use_loc_size = FALSE ) %>%
+  
+  # Slide 6
+  add_slide(., layout= "Content with Caption", master= "Circuit") %>%
+  ph_with_text(type = "title", index=1,str = "Monthly Boxplot's for historical closings") %>%
+  ph_with_text(type = "body", index=2,str = cap3) %>%  
+  ph_with(., value= external_img("~/R/Projects/OfficeR_stocks_report/monthly.png", width = 7, height = 5),
+          location = ph_location_type(type = "body", index=1), use_loc_size = FALSE ) %>%
+  
+  # Slide 7
+  add_slide(., layout= "Content with Caption", master= "Circuit") %>%
+  ph_with_text(type = "title", index=1,str = "Forecasted Closing Prices using Facebook Prophet Package") %>%
+  ph_with_text(type = "body", index=2,str = cap4) %>%
+  ph_with(., value= external_img("~/R/Projects/OfficeR_stocks_report/forecast.png", width = 7, height = 5),
+          location = ph_location_type(type = "body", index=1), use_loc_size = FALSE ) %>%    
+  
+  # Slide 8
+  add_slide(., layout= "Content with Caption", master= "Circuit") %>%
   ph_with_text(type = "title", index=1,str = "Forecasted Components using Facebook Prophet") %>%
-  ph_with_text(type = "body", index=1,str = cap5) %>%
-  ph_with(., value= external_img("/Users/daviddemand/R/Projects/SimpleStocksReport/components.png", width = 6.75, height = 5.33),
-          location = ph_location_type(type = "body", index=2), use_loc_size = FALSE ) %>%
+  ph_with_text(type = "body", index=2,str = cap5) %>%
+  ph_with(., value= external_img("~/R/Projects/OfficeR_stocks_report/components.png", width = 7, height = 5),
+          location = ph_location_type(type = "body", index=1), use_loc_size = FALSE ) %>%
   print(., target="StocksReport.pptx") %>%
   invisible()
